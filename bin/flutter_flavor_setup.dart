a41abaf63c177248a2ca6cc28e92baece6f3263c
abdallah abusnineh <124106124+Abdallahabusnineh@users.noreply.github.com>
2025-11-04 17:35:01 +0300

    make the package discontinued

--- bin/flutter_flavor_setup.dart
+++ bin/flutter_flavor_setup.dart
@@ -1,12 +1,13 @@
 #!/usr/bin/env dart
 
-import 'package:flutter_flavor_setup/flutter_flavor_setup.dart';
-
 /// Entry point for the Flutter Flavor Setup CLI tool
 ///
 /// Run with: dart run flutter_flavor_setup.dart
 /// Or: dart bin/flutter_flavor_setup.dart
 Future<void> main() async {
-  final command = FlavorSetupCommand();
-  await command.execute();
+  throw UnsupportedError(
+    'flutter_flavor_setup is discontinued.\n'
+    'Please migrate to flutter_flavor_manager:\n'
+    'https://pub.dev/packages/flutter_flavor_manager',
+  );
 }
